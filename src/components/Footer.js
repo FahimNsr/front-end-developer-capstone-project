294b8a90d3f987859aa3f80e13fa63c346a9c8ca
Fahim Nasirzadeh <nasirzadeh@punkt.de>
2025-11-29 13:46:32 +0100

    Setting up the HTML document

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <h4>Little Lemon</h4>
+        <p>© 2024 Little Lemon Restaurant. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main>
+      {/* Main content will go here */}
+    </main>
+  );
+}
+
+export default Main;
+
