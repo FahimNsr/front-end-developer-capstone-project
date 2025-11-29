ad84d633483e556b589e718b12b7647d6d06a870
Fahim Nasirzadeh <nasirzadeh@punkt.de>
2025-11-29 23:31:40 +0100

    Connecting the bookings page to the API

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Load API script if not already loaded
+if (!window.fetchAPI) {
+  fetch('https://raw.githubusercontent.com/courseraap/capstone/main/api.js')
+    .then(response => response.text())
+    .then(scriptContent => {
+      const modifiedScript = scriptContent
+        .replace(/const fetchAPI = /g, 'window.fetchAPI = ')
+        .replace(/const submitAPI = /g, 'window.submitAPI = ')
+        .replace(/const seededRandom = /g, 'window.seededRandom = ');
+      eval(modifiedScript);
+    })
+    .catch(() => {});
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/utils/timesReducer.js
+++ src/utils/timesReducer.js
@@ -1,14 +1,19 @@
 // Initialize available times
 export function initializeTimes() {
+  const today = new Date();
+  if (window.fetchAPI && typeof window.fetchAPI === 'function') {
+    return window.fetchAPI(today);
+  }
   return ['17:00', '18:00', '19:00', '20:00', '21:00'];
 }
 
 // Update available times based on selected date
 export function updateTimes(state, action) {
-  // For now, return the same available times regardless of the date
-  // This will be updated in future steps to filter based on date
   if (action.type === 'UPDATE_TIMES') {
-    // Currently returns the same times, but action.date is available for future use
+    if (window.fetchAPI && typeof window.fetchAPI === 'function' && action.date) {
+      const dateObj = new Date(action.date);
+      return window.fetchAPI(dateObj);
+    }
     return state;
   }
   return state;
